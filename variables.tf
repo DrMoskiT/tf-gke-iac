fadeeeb0bb71659d7e2dabef84f9a82c0b709ccd
DrMoskiT <32967637+DrMoskiT@users.noreply.github.com>
2025-12-11 19:09:19 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "GOOGLE_PROJECT" {
+  description = "ID GCP проекту"
+  type        = string
+}
 
+variable "GOOGLE_REGION" {
+  description = "Регіон GCP для GKE (наприклад, us-central1)"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "GKE_NUM_NODES" {
+  description = "Кількість нод у node pool"
+  type        = number
+  default     = 2
+}
